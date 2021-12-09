6b42ddeec789ba918dad0c50cd9093393984fb47
Awais <awaisdesigner50@gmail.com>
2021-12-08 21:39:29 -0800

    Js programs

---
+++ script.js
@@ -0,0 +1,370 @@
+// 1. Write a js program to find maximum between two numbers.
+
+var num1 = parseInt(prompt("enter first number"))
+var num2 = parseInt(prompt("enter second number"))
+
+
+var result = num1>num2
+if (num1>num2){
+   console.log("Greatest number", num1);
+}
+else {
+   console.log("Greatest number", num2);
+}
+
+
+
+
+
+// 2. Write a js program to find maximum between three numbers.  
+
+// var num1 = parseInt(prompt("enter first number"))
+// var num2 = parseInt(prompt("enter second number"))
+// var num3 = parseInt(prompt("enter third number"))
+// if (num1>num2 && num1>num3) {
+//     console.log("Greatest number is", num1);
+// }
+// else{
+//     if (num2>num1 && num2>num3) {
+//         console.log("Greatest number is", num2);
+//         }
+//    else{
+//     console.log("Greatest number is", num3);
+//    }
+// }
+
+
+
+
+// 3. Write a js program to check whether a number is negative, positive or zero.
+
+// var n = parseInt(prompt("enter any number"))
+// if (n<0) {
+//     console.log("Number is nagative", n);
+// }
+
+// else{
+//     if (n==0) {
+//         console.log("Number is zero", n);
+//     }
+
+//     else{
+//         console.log("Number is positive", n);
+//     }
+// }
+
+
+
+// 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+
+// var n = parseInt(prompt("Enter any number"))
+// if (n%5==0 && n%11==0) {
+//     console.log("Number is divisible", n);
+// }
+// else{
+//     console.log("Number is not divisible", n);
+// }
+
+
+
+
+// 5. Write a js program to check whether a number is even or odd.
+
+// var n = parseInt(prompt("Enter any number"))
+// if (n%2==0) {
+//     console.log("Number is even", n);
+// } 
+// else{
+//     console.log("Number is odd", n);
+// }
+
+
+
+// 6. Write a js program to check whether a year is leap year or not.
+
+// var year = parseInt(prompt("Enter any year"))
+// if (year%4==0) {
+//     console.log("This is leap year", year );
+// }
+// else{
+//     console.log("This is not leap year", year);
+// }
+
+
+
+
+// 7. Write a js program to check whether a character is alphabet or not.
+// if (alphabet >= "a" && alphabet <= "z" || alphabet >= "A" && alphabet <= "Z")
+
+// var ch = prompt("Enter any Character")
+// if (ch == 'a' || ch == 'A') {
+//     console.log("This is alphabet", ch);
+// }
+// else {
+//     if (ch == 'b' || ch == 'B') {
+//         console.log("This is alphabet", ch);
+//     }
+
+//     else {
+//         if (ch == 'c' || ch == 'C') {
+//             console.log("This is alphabet", ch);
+//         }
+//         else {
+//             if (ch == 'd' || ch == 'D') {
+//                 console.log("This is alphabet", ch);
+//             }
+
+//             else {
+//                 if (ch == 'e' || ch == 'E') {
+//                     console.log("This is alphabet", ch);
+//                 }
+//                 else {
+//                     if (ch == 'f' || ch == 'F') {
+//                         console.log("This is alphabet", ch);
+//                     }
+
+//                     else {
+//                         if (ch == 'g' || ch == 'G') {
+//                             console.log("This is alphabet", ch);
+//                         }
+//                         else {
+//                             if (ch == 'h' || ch == 'H') {
+//                                 console.log("This is alphabet", ch);
+//                             }
+
+//                             else {
+//                                 if (ch == 'i' || ch == 'I') {
+//                                     console.log("This is alphabet", ch);
+//                                 }
+
+//                                 else {
+//                                     if (ch == 'j' || ch == 'J') {
+//                                         console.log("This is alphabet", ch);
+//                                     }
+//                                     else {
+//                                         if (ch == 'k' || ch == 'K') {
+//                                             console.log("This is alphabet", ch);
+//                                         }
+//                                         else {
+//                                             if (ch == 'l' || ch == 'L') {
+//                                                 console.log("This is alphabet", ch);
+//                                             }
+//                                             else {
+//                                                 if (ch == 'm' || ch == 'M') {
+//                                                     console.log("This is alphabet", ch);
+//                                                 }
+//                                                 else {
+//                                                     if (ch == 'n' || ch == 'N') {
+//                                                         console.log("This is alphabet", ch);
+//                                                     }
+//                                                     else {
+//                                                         if (ch == 'o' || ch == 'O') {
+//                                                             console.log("This is alphabet", ch);
+//                                                         }
+//                                                         else {
+//                                                             if (ch == 'p' || ch == 'P') {
+//                                                                 console.log("This is alphabet", ch);
+//                                                             }
+//                                                             else {
+//                                                                 if (ch == 'q' || ch == 'Q') {
+//                                                                     console.log("This is alphabet", ch);
+//                                                                 }
+//                                                                 else {
+//                                                                     if (ch == 'r' || ch == 'R') {
+//                                                                         console.log("This is alphabet", ch);
+//                                                                     }
+//                                                                     else {
+//                                                                         if (ch == 's' || ch == 'S') {
+//                                                                             console.log("This is alphabet", ch);
+//                                                                         }
+//                                                                         else {
+//                                                                             if (ch == 't' || ch == 'T') {
+//                                                                                 console.log("This is alphabet", ch);
+//                                                                             }
+//                                                                             else {
+//                                                                                 if (ch == 'u' || ch == 'U') {
+//                                                                                     console.log("This is alphabet", ch);
+//                                                                                 }
+//                                                                                 else {
+//                                                                                     if (ch == 'v' || ch == 'V') {
+//                                                                                         console.log("This is alphabet", ch);
+//                                                                                     }
+//                                                                                     else {
+//                                                                                         if (ch == 'w' || ch == 'W') {
+//                                                                                             console.log("This is alphabet", ch);
+//                                                                                         }
+//                                                                                         else {
+//                                                                                             if (ch == 'x' || ch == 'X') {
+//                                                                                                 console.log("This is alphabet", ch);
+//                                                                                             }
+//                                                                                             else {
+//                                                                                                 if (ch == 'y' || ch == 'Y') {
+//                                                                                                     console.log("This is alphabet", ch);
+//                                                                                                 }
+//                                                                                                 else {
+//                                                                                                     if (ch == 'z' || ch == 'Z') {
+//                                                                                                         console.log("This is alphabet", ch);
+//                                                                                                     }
+
+//                                                                                                     else {
+//                                                                                                         console.log("Invalid !! plz enter any character");
+//                                                                                                     }
+//                                                                                                 }
+//                                                                                             }
+//                                                                                         }
+//                                                                                     }
+//                                                                                 }
+//                                                                             }
+//                                                                         }
+//                                                                     }
+//                                                                 }
+//                                                             }
+//                                                         }
+//                                                     }
+//                                                 }
+//                                             }
+//                                         }
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+//   8. Write a js program to input any alphabet and check whether it is vowel or consonant.
+
+//   var alpha = prompt("Enter any alphabet")
+//   if (alpha=='a' || alpha=='A') {
+//       console.log("This is vowel", alpha);
+//   }
+//     else{
+//         if (alpha=='e' || alpha=='E') {
+//         console.log("This is vowel", alpha);
+//         }
+
+//     else{
+//         if (alpha=='i' || alpha=='I') {
+//         console.log("This is vowel", alpha);
+//         }
+//     else{
+//         if (alpha=='o' || alpha=='O') {
+//           console.log("This is vowel", alpha);
+//         }
+
+//     else{
+//         if (alpha=='u' || alpha=='U') {
+//               console.log("This is vowel", alpha);
+//         }
+//     else{
+//         console.log("Invalid", alpha);
+//     } 
+//     }
+//     }
+//     }
+//     }  
+
+
+
+
+
+
+// 11. Write a js program to input week number and print week day.
+
+// var num = parseInt(prompt("Enter week number"))
+// if (num == 1) {
+//     console.log("This is Monday", num);
+// }
+// else {
+//     if (num == 2) {
+//         console.log("This is Tue", num);
+//     }
+
+//     else {
+//         if (num == 3) {
+//             console.log("This is Wed", num);
+//         }
+
+
+//         else {
+//             if (num == 4) {
+//                 console.log("This is Thurs", num);
+//             }
+
+//             else {
+//                 if (num == 5) {
+//                     console.log("This is Fri", num);
+//                 }
+
+//                 else {
+//                     if (num == 6) {
+//                         console.log("This is Sat", num);
+//                     }
+
+//                     else {
+//                         if (num == 7) {
+//                             console.log("This is Sun", num);
+//                         }
+
+//                         else {
+//                             console.log("This is Invalid", num);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+// 12. Write a js program to input month number and print number of days in that month.
+
+// var n = parseInt(prompt("ENTER A MONTH NUMBER"))
+// if (n == 1) {
+//     console.log("JANUARY/n 31");
+// }
+// else if (n == 2) {
+//     console.log("FEBRUARY  28");
+// }
+// else if (n == 3) {
+//     console.log("MARCH 31");
+// }
+// else if (n == 4) {
+//     console.log("APRIL 30");
+// }
+// else if (n == 5) {
+//     console.log("MAY 31");
+// }
+// else if (n == 6) {
+//     console.log("JUNE 30");
+// }
+// else if (n == 7) {
+//     console.log("JULY 31");
+// }
+// else if (n == 8) {
+//     console.log("AUGUST 31");
+// }
+// else if (n == 9) {
+//     console.log("SEPTEMBER 31");
+// }
+// else if (n == 10) {
+//     console.log("OCTOBER 31");
+// }
+// else if (n == 11) {
+//     console.log("NOVEMBER 30");
+// } else if (n == 12) {
+//     console.log("DECEMBER 31");
+// }
+// else {
+//     console.log("This is invalid");
+// }
+
+
+
+
